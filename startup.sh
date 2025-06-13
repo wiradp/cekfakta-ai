585550e9a39c43a316b629164aa31ede5f57ae49
Wiradp <wiradp@yahoo.com>
2025-06-13 22:06:19 +0700

    Comment out pip installation commands in startup.sh for clarity

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install requirements locally into site-packages (home/site/wwwroot)
-pip install --upgrade pip
-pip install -r requirements.txt
+# pip install --upgrade pip
+# pip install -r requirements.txt
 
 # Start gunicorn
 gunicorn app:app --bind=0.0.0.0 --timeout 600
